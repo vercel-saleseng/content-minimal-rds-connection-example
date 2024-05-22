9d8844619490013b9ab9783357d2043144e79c08
Aman Azad <aman.s.azad@gmail.com>
2024-05-22 15:50:06 -0700

    set up bastion instance and security group to manage db from local machine

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -14,6 +14,16 @@ variable "lambdasVersion" {
   description = "version of the lambdas zip on S3"
 }
 
+variable "my_ip" {
+  type        = string
+  description = "Your IP address"
+}
+
+variable "bastion_key_pair" {
+  type        = string
+  description = "The key pair to use for the bastion instance"
+}
+
 locals {
   private_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnet_cidrs  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
@@ -43,6 +53,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 ### Networking + Security
+### Set up VPC, subnets, security groups, and IAM roles
 
 module "vpc" {
   source               = "terraform-aws-modules/vpc/aws"
@@ -69,17 +80,48 @@ resource "aws_security_group" "rds" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = local.private_subnet_cidrs
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    cidr_blocks     = local.private_subnet_cidrs
+    security_groups = [aws_security_group.rds_public.id]
   }
 
   egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "vercel_rds"
+  }
+}
+
+resource "aws_security_group" "rds_public" {
+  name   = "vercel_rds_public"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip}/32"]
+  }
+
+  ingress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = local.private_subnet_cidrs
+    cidr_blocks = ["${var.my_ip}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -99,10 +141,10 @@ resource "aws_security_group" "lambda" {
   }
 
   egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = local.private_subnet_cidrs
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -221,13 +263,14 @@ resource "aws_db_instance" "vercel" {
   username               = "vercel"
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.vercel.name
-  vpc_security_group_ids = [aws_security_group.rds.id]
+  vpc_security_group_ids = [aws_security_group.rds.id, aws_security_group.rds_public.id]
   parameter_group_name   = aws_db_parameter_group.vercel.name
   publicly_accessible    = false
   skip_final_snapshot    = true
 }
 
 ### RDS Proxy
+### Used to manage serverless connections to the RDS instance
 
 resource "aws_db_proxy" "vercel_rds_proxy" {
   name                   = "vercel-rds-proxy"
@@ -283,6 +326,7 @@ resource "aws_lambda_function" "crud_lambda" {
 }
 
 ### API Gateway 
+### Exposes the Lambda function to the internet, the rest of our infra will sit inside our VPC
 
 resource "aws_apigatewayv2_api" "todo_api" {
   name          = "todo_api"
@@ -296,16 +340,14 @@ resource "aws_apigatewayv2_stage" "todo_api_prod" {
 }
 
 resource "aws_apigatewayv2_integration" "crud_lambda" {
-  api_id = aws_apigatewayv2_api.todo_api.id
-
+  api_id             = aws_apigatewayv2_api.todo_api.id
   integration_uri    = aws_lambda_function.crud_lambda.invoke_arn
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
 }
 
 resource "aws_apigatewayv2_route" "crud_lambda" {
-  api_id = aws_apigatewayv2_api.todo_api.id
-
+  api_id    = aws_apigatewayv2_api.todo_api.id
   route_key = "POST /crud"
   target    = "integrations/${aws_apigatewayv2_integration.crud_lambda.id}"
 }
@@ -321,6 +363,7 @@ resource "aws_lambda_permission" "crud_lambda" {
 }
 
 ### S3
+### Store the terraform state in S3
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-state-vercel"
@@ -331,6 +374,22 @@ resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
 
   versioning_configuration {
     status = "Enabled"
+  }
+}
+
+### EC2
+### Bastion instance to connect to the RDS instance
+
+resource "aws_instance" "bastion" {
+  ami                         = "ami-0bb84b8ffd87024d8"
+  instance_type               = "t2.micro"
+  subnet_id                   = module.vpc.public_subnets[0]
+  vpc_security_group_ids      = [aws_security_group.rds.id, aws_security_group.rds_public.id]
+  key_name                    = var.bastion_key_pair
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "vercel_bastion"
   }
 }
 
@@ -355,7 +414,12 @@ output "rds_username" {
 }
 
 
-output "base_url" {
+output "api_gateway_url" {
   description = "Base URL for API Gateway."
   value       = aws_apigatewayv2_stage.todo_api_prod.invoke_url
-o
+}
+
+output "bastion_ip" {
+  description = "Bastion IP address"
+  value       = aws_instance.bastion.public_ip
+}
